75d7f4468383425207b9902de9e1ec0d8130a166
vitorqt <vitorqt@hotmail.com>
2019-12-16 14:14:56 -0300

    Adiciona component criar poll

---
+++ src/components/CreatePoll.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="main">
+    <Header />
+    <div class="container">
+      <h1>
+        Create a<span> Poll</span>
+      </h1>
+      <label class="mg-top">Poll question</label>
+      <input class="big-input" type="text" placeholder="Type your question here..." />
+      <label class="mg-top">Poll question</label>
+      <input class="small-input" type="text" placeholder="Enter poll option..." />
+      <label class="mg-top">Poll question</label>
+      <input class="small-input" type="text" placeholder="Enter poll option..." />
+      <label class="mg-top">Poll question</label>
+      <input class="small-input" type="text" placeholder="Enter poll option..." />
+
+      <div class="container-options">
+        <button class="btn-add">Add another option</button>
+
+        <div class="options">
+          <p>Improve spam prevention</p>
+          <p>Allow multiple poll answers</p>
+        </div>
+      </div>
+
+      <router-link to="">
+        <button class="btn-create">Create your poll</button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+</script>
+
+
+<style scoped lang="scss">
+@import "../assets/scss/variables"; 
+
+.container {
+  position: absolute;
+  width: 100%;
+  max-width: 500px;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -40%);
+
+  h1 {
+    color: $primary-color;
+    letter-spacing: $letter-spacing-less;
+
+    span { color: $secundary-color; }
+  }
+
+  label { color: $secundary-color; font-size: 18px; }
+
+  input {
+    margin: 10px 0 25px 0;  
+    display: block;
+    background-color: $input-grey;
+    color: #000000;
+    font-size: 15px;
+    border: none;
+    border-radius: 5px;
+    width: 100%;
+    max-width: 450px;
+  }
+  input:focus, select:focus {
+    border: 1px solid $primary-color;
+    opacity: .5;
+    outline: 0;
+    -webkit-box-shadow: none !important;
+    box-shadow: none !important;
+  }
+  .big-input { padding: 20px; }
+  .small-input { padding: 10px 20px; }
+  input::placeholder { color: #000; }
+
+  .mg-top {
+    margin-top: 20px;
+  }
+
+  .container-options {
+    display: flex;
+    justify-content: space-between;
+
+    .options {
+      padding-top: 10px;
+      color: $primary-color;
+    }
+  }
+
+  button {
+    border: none;
+    height: 50px;
+    width: 200px;
+    color: #E4E0EE;
+    transition: all .5s;
+    cursor: pointer;
+  }
+
+  .btn-add {
+    margin-top: 20px;
+    font-size: 14px;
+    background-color: #565068;
+  }
+  .btn-add:hover {
+    background-color: $secundary-color-hover;
+  }
+  .btn-create {
+    font-size: 18px;
+    background-color: #50AE6E;
+  }
+  .btn-create:hover {
+    background-color: $primary-color-hover;
+  }
+}
+
+// RESPONSIVIDADE - MEDIA QUERYS
+
+@media only screen and (max-width: 425px) {
+  .container {
+    padding: 20px;
+
+    .container-options {
+      display: block;
+    }
+  
+    button {
+      margin-top: 10px;
+    }
+  }
+
+  footer {
+    padding: 15px;
+  }
+}
+</style>
